2b2e3433a743454d631a94a3c0b0aaf926f6743d
Axel KLEIN <axelklein2@gmail.com>
2021-10-19 14:18:16 +0200

    oui

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const auth = require('./a.js');
-console.log(auth.test);
+//const auth = require('./a.js');
+//console.log(auth.test);
 
 const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    //res.send("Hellow World!");
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.json({ message : "Hello World" }),
+    //res.sendFile(path.join(__dirname + '/views/index.html'));
+    
 });
+
+app.get('/store',(req,res)=>{
+
+});
+
+app.get('/store/:search', (req,res)=>{
+
+})
+
+app.get('/games', (req,res)=>{
+
+})
+
+app.get('/games/:id', (req,res)=>{
+
+})
+
+app.get('')
+
+
 
 //app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
-  
