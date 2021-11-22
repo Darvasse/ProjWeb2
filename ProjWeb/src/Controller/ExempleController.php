e61521b14e41e8e4b820bfb736feae3d8466fd91
Lilian <liliangst@outlook.fr>
2021-11-22 17:37:17 +0100

    Inchallah ca marche

--- ProjWeb/src/Controller/ExempleController.php
+++ ProjWeb/src/Controller/ExempleController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ExempleController
 {
+    /**
+     * @Route("/")
+     **/
     public function index()
     {
         return new Response("Hello World !");
---
+++ ProjWeb/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/' => [[['_route' => 'app_exemple_index', '_controller' => 'App\\Controller\\ExempleController::index'], null, null, null, false, false, null]],
+        '/home' => [[['_route' => 'app_steam_homehandler', '_controller' => 'App\\Controller\\SteamController::homeHandler'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
